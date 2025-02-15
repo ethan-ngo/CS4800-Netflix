acaeb17a9b34b5eaaced45bea85c63714da82d0b
MoneyEt <ethanngo3539@gmail.com>
2025-02-15 11:18:42 -0800

    added displayDB component showing mock user info

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import cors from "cors";
 import users from "./routes/users.js";
-import test from "./routes/test.js"
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/users", users);
-app.use("/test", test);
+app.use("/users", users)
 
 // start the Express server
 app.listen(PORT, () => {
