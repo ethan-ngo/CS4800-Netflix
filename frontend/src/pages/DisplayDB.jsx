2658d5df94a708476f7fe8c28e4eedd8692a986d
MoneyEt <ethanngo3539@gmail.com>
2025-02-19 10:58:17 -0800

    fixed case sensitivity

--- frontend/src/pages/DisplayDB.jsx
+++ frontend/src/pages/DisplayDB.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import "./displayDB.css";
+import "./DisplayDB.css";
 
 function DisplayDB() {
   const [username, setUsername] = useState("");
