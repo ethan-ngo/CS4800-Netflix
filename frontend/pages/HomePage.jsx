9844e80aa2beb06100d4bc515e4ea0ca5f37dfe7
josh2816 <yijoshuag@gmail.com>
2025-03-12 15:49:20 -0700

    adding to the home page

--- frontend/pages/HomePage.jsx
+++ frontend/pages/HomePage.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TextInput, Button, Alert, StyleSheet, Platform } from 'react-native'
+import { useNavigate, Link } from 'react-router-dom'
 
 const HomePage = () => {
+  
+  if (Platform.OS === 'web') {
+    return(
+      <div style={webStyles.navBar}>
+        <h1 style={webStyles.Logo}>
+          DomainFilms
+        </h1>
+      </div>
+    )
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
@@ -21,5 +32,22 @@ const styles = StyleSheet.create({
     color: 'black',
   },
 })
-
+const webStyles={
+  navBar:{
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%', 
+  backgroundColor: 'black', 
+  display: 'flex',
+  justifyContent: 'space-between', 
+  gap: '20px', 
+  //boxshadow:w 0 4px 6px rgba(0, 0, 0, 0.1),
+  //z-index: 1000, /* Ensures it stays above other content */
+  },
+  Logo:{
+    justifyContent: 'left',
+    color: 'purple',
+  },
+}
 export default HomePage
