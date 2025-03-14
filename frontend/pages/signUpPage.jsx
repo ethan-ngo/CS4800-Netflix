d9a1f1d5d2afd325dc634d1e9ded761b6477bfa9
MoneyEt <ethanngo3539@gmail.com>
2025-03-13 18:26:45 -0700

    Added navigation to signup page and changed styling for forgot password page

--- frontend/pages/signUpPage.jsx
+++ frontend/pages/signUpPage.jsx
@@ -10,6 +10,7 @@ const SignUpPage = () => {
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [response, setResponse] = useState(null)
+  const navigate = useNavigate()
 
   // validations and confirmation for sign-up
   const handleSignUp = async () => {
@@ -75,6 +76,8 @@ const SignUpPage = () => {
       const data = await res.json()
       setResponse(data)
       console.log('New user with username: ' + name + ' and email: ' + email + ' has been created')
+      navigate('/login')
+
     } catch (error) {
       console.error('Error:', error)
       setResponse({ error: 'Failed to submit data' })
