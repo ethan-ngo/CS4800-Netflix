81aaf6999f0ab5a9de5f771410da5e3d6876b494
BennLL <zihaoluo153@gmail.com>
2025-03-19 13:48:41 -0700

    Added native code for media details

--- frontend/App.js
+++ frontend/App.js
@@ -13,6 +13,7 @@ import ForgotPasswordNative from './pages/ForgotPasswordNative'
 import HomePageNative from './pages/HomePageNative'
 import ResetPasswordNative from './pages/ResetPasswordNative'
 import ProfilePageNative from './pages/ProfilePageNative'
+import MediaDetailsNative from './pages/mediaDetailsNative'
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Header from './components/Header';
@@ -36,6 +37,7 @@ export default function App() {
           <Stack.Screen name="Home" component={HomePageNative} />
           <Stack.Screen name="SignUp" component={SignUpPageNative} />
           <Stack.Screen name="Profile" component={ProfilePageNative} />
+          <Stack.Screen name="MediaDetailsNative" component={MediaDetailsNative} />
         </Stack.Navigator>
       </View>
     </NavigationContainer>
