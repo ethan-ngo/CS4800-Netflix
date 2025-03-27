2160dfc0cb2246754118206ad132d07788524533
MoneyEt <ethanngo3539@gmail.com>
2025-03-27 16:15:12 -0700

    updated aws config

--- frontend/aws-config.js
+++ frontend/aws-config.js
@@ -2,12 +2,12 @@ import AWS from "aws-sdk";
 
 // AWS S3 Configuration
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY, // Stored in .env
-  secretAccessKey: process.env.AWS_SECRET, // Stored in .env
-  region: "us-east-2", // Change to your AWS region
+  accessKeyId: process.env.APP_AWS_ACCESS_KEY, // Stored in .env
+  secretAccessKey: process.env.APP_AWS_SECRET, // Stored in .env
+  region: "us-west-1", // Change to your AWS region
   signatureVersion: "v4",
 });
 
-const BUCKET_NAME = "cs4800domain"; // Change to your bucket name
+const BUCKET_NAME = "cs4800lebron"; // Change to your bucket name
 
 export { s3, BUCKET_NAME };
