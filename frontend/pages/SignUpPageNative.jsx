50d278d501379c8d21a658bc3d7ec5ace5a46b6c
josh2816 <yijoshuag@gmail.com>
2025-03-27 21:29:05 -0700

    backgroundimages

--- frontend/pages/SignUpPageNative.jsx
+++ frontend/pages/SignUpPageNative.jsx
@@ -7,6 +7,7 @@ import {
   Alert,
   StyleSheet,
   ScrollView,
+  Image,
 } from 'react-native'
 import { validateEmail } from '../utils/validateEmail'
 import theme from '../utils/theme'
@@ -77,6 +78,11 @@ const SignUpPageNative = ({ navigation }) => {
   return (
     <View style={styles.container}>
       {loading && <LoadingOverlay visible={loading} />}
+      <Image 
+          source={{ uri: "https://wallpapers.com/images/hd/movie-poster-background-wg5mxe6b7djul0a8.jpg" }} 
+          style={styles.backgroundImage} 
+          resizeMode="cover"
+        />
       <Header />
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.form}>
@@ -183,6 +189,12 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     fontSize: 14,
     color: 'gray',
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    opacity: 0.7,
   },
 })
 
