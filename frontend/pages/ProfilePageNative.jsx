2a9e843697ecc2527d562291829a6ed57e27ddd5
jmoran17 <Jrmoran@cpp.edu>
2025-04-16 11:37:26 -0700

    fixed page formatting

--- frontend/pages/ProfilePageNative.jsx
+++ frontend/pages/ProfilePageNative.jsx
@@ -225,7 +225,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
+    padding: 0,
    // backgroundColor: 'white',
   },
   form: {
